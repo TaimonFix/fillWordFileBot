d2389f47f579e6d971b6830c8fa8e25d06864ed1
bratyshevTD <bratyshev2013@mail.ru>
2025-02-16 22:28:32 +0300

    Добавлена Docker-Контейнеризация

--- src/main/java/ru/snptech/fillWordFileBot/FillWordFileBotApplication.java
+++ src/main/java/ru/snptech/fillWordFileBot/FillWordFileBotApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class 	FillWordFileBotApplication {
+public class FillWordFileBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FillWordFileBotApplication.class, args);
